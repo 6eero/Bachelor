fd5c7fab4c6f9a4e88ca4a184383d90913e2eb94
6eero <gerotto.alessandro08@gmail.com>
2024-08-17 12:12:49 +0200

    added ex

---
+++ Linguaggi-di-Programmazione/esercizi-haskell/2.4_trova-primi-due-dispari.hs
@@ -0,0 +1,12 @@
+-- Scrivere una funzione che calcola i 2 minori elementi dispari di una lista (se esistono). 
+-- Ad esempio minOdd([2,3,4,6,8,7,5]) riduce a (3,5)
+
+
+-- uso quicksort implementato nell'esercizio precedente
+quicksort :: [Int] -> [Int]
+quicksort [] = []
+quicksort [x] = [x]
+quicksort (pivot:xs) =  quicksort [x | x <- xs, x <= pivot] ++ [pivot] ++ quicksort [x | x <- xs, x > pivot]
+
+minOdd :: [Int] -> [Int]
+minOdd xs = take 2 $ quicksort $ filter odd xs
