5728eb4e516a995d0fe3fff3a6e89aa515aa9a27
6eero <gerotto.alessandro08@gmail.com>
2024-08-25 12:19:27 +0200

    .

--- Linguaggi-di-Programmazione/esercizi-haskell/esercizi-esami.hs
+++ Linguaggi-di-Programmazione/esercizi-haskell/esercizi-esami.hs
@@ -14,7 +14,7 @@ isSublist sub lst@(x:xs)
 
 -- una funzione che data una lista determina se questa e' palindroma;
 isPalyndrome :: [Int] -> Bool
-isPalyndrome [] = True
+isPalyndrome [] = False
 isPalyndrome [x] = True
 isPalyndrome (x:xs) =
   (x == last xs) && isPalyndrome (init xs)
@@ -29,18 +29,3 @@ getLenOfTheLastPalyndrome (x:xs) =
     else getLenOfTheLastPalyndrome xs
 
 -- una funzione che data una lista determina la lunghezza della piu' lunga sottolista palindroma.
-fun1 :: [Int] -> Int
-fun1 [] = 0
-fun1 [x] = 1
-fun1 (x:xs) =
-  if x == last xs && isPalyndrome (x:xs)
-    then length (x:xs)
-    else fun1 xs
-
-fun2 :: [Int] -> Int
-fun2 [] = 0
-fun2 [x] = 1
-fun2 (x:xs) =
-  if x == last xs && isPalyndrome (x:xs)
-    then length (x:xs)
-    else fun2 (init xs)
